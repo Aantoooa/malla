47e4fce1a97299eeeb38372ea49d11fc691994bd
Aantoooa <antonellabellof@gmail.com>
2025-07-14 22:13:22 -0400

    Update cursos.js

--- cursos.js
+++ cursos.js
@@ -1,50 +1,18 @@
 const COURSES = [
   {
-    "id": "IME027",
-    "name": "PRECÁLCULO I",
+    "id": "PRUEBA_1",
+    "name": "Asignatura de Prueba 1",
     "level": 1,
     "credits": 3,
-    "area": "MATEMATICAS Y ESTADISTICA",
+    "area": "Test",
     "prerequisites": []
   },
   {
-    "id": "IME028",
-    "name": "INTRODUCCIÓN AL ÁLGEBRA",
-    "level": 1,
-    "credits": 3,
-    "area": "MATEMATICAS Y ESTADISTICA",
-    "prerequisites": []
-  },
-  {
-    "id": "IME029",
-    "name": "RECÁLCULO II",
-    "level": 1,
-    "credits": 3,
-    "area": "MATEMATICAS Y ESTADISTICA",
-    "prerequisites": []
-  },
-  {
-    "id": "IME030",
-    "name": "ÁLGEBRA",
-    "level": 1,
-    "credits": 3,
-    "area": "MATEMATICAS Y ESTADISTICA",
-    "prerequisites": ["IME028"]
-  },
-  {
-    "id": "ICQ050",
-    "name": "INTRODUCCIÓN A LA QUÍMICA",
-    "level": 1,
+    "id": "PRUEBA_2",
+    "name": "Asignatura de Prueba 2",
+    "level": 2,
     "credits": 5,
-    "area": " CIENCIAS QUIMICAS Y REC. NAT.",
-    "prerequisites": [] 
-  },
-  {
-    "id": "DFI052",
-    "name": "HABILIDADES COMUNICATIVAS EN INGENIERÍA",
-    "level": 1,
-    "credits": 3,
-    "area": "COORDINACION FORMACIÓN GENERAL E IDIOMAS",
-    "prerequisites": [] 
+    "area": "Test",
+    "prerequisites": ["PRUEBA_1"]
   }
 ];
