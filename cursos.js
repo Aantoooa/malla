9a68acf60d952af03ca67c64cb793f72ed38a3d8
Aantoooa <antonellabellof@gmail.com>
2025-07-14 22:55:57 -0400

    Update cursos.js

--- cursos.js
+++ cursos.js
@@ -111,4 +111,134 @@ const COURSES = [
     area: "DECANATO FACULTAD DE INGENIERIA",
     prerequisites: []
   }
+[
+  {
+    "id": "IME298",
+    "name": "CÁLCULO MULTIVARIABLE",
+    "level": 3,
+    "credits": 5.0,
+    "area": "DEPTO. DE MATEMATICAS Y ESTADISTICA",
+    "prerequisites": ["IME048"]
+  },
+  {
+    "id": "IME299",
+    "name": "ÁLGEBRA LINEAL",
+    "level": 3,
+    "credits": 5.0,
+    "area": "DEPTO. DE MATEMATICAS Y ESTADISTICA",
+    "prerequisites": ["IME030"]
+  },
+  {
+    "id": "ICF177",
+    "name": "FÍSICA II",
+    "level": 3,
+    "credits": 6.0,
+    "area": "DEPTO. DE CIENCIAS FISICAS",
+    "prerequisites": ["IME048", "ICF115"]
+  },
+  {
+    "id": "ING400",
+    "name": "ELECTIVO DE INGENIERIA",
+    "level": 3,
+    "credits": 3.0,
+    "area": "FACULTAD DE INGENIERIA Y CIENCIAS",
+    "prerequisites": []
+  },
+  {
+    "id": "ICC157",
+    "name": "TALLER DE PROGRAMACIÓN",
+    "level": 3,
+    "credits": 3.0,
+    "area": "DEPTO. CS. COMPUTACION E INFORMATICA",
+    "prerequisites": ["ING104"]
+  },
+  {
+    "id": "ING180",
+    "name": "ECODISEÑO",
+    "level": 3,
+    "credits": 4.0,
+    "area": "DECANATO FACULTAD DE INGENIERIA",
+    "prerequisites": []
+  },
+  {
+    "id": "ICC490",
+    "name": "PROGRAMACION ORIENTADA A OBJETOS",
+    "level": 4,
+    "credits": 5.0,
+    "area": "DEPTO. CS. COMPUTACION E INFORMATICA",
+    "prerequisites": ["ICC157"]
+  },
+  {
+    "id": "IME317",
+    "name": "ECUACIONES DIFERENCIALES",
+    "level": 4,
+    "credits": 6.0,
+    "area": "DEPTO. DE MATEMATICAS Y ESTADISTICA",
+    "prerequisites": ["IME299"]
+  },
+  {
+    "id": "ICF489",
+    "name": "FISICA III",
+    "level": 4,
+    "credits": 5.0,
+    "area": "DEPTO. DE CIENCIAS FISICAS",
+    "prerequisites": ["IME298", "ICF177"]
+  },
+  {
+    "id": "ICC492",
+    "name": "TALLER DE COMPUTACION",
+    "level": 4,
+    "credits": 4.0,
+    "area": "DEPTO. CS. COMPUTACION E INFORMATICA",
+    "prerequisites": ["ING400"]
+  },
+  {
+    "id": "I95310",
+    "name": "ELECTIVO DE FORMACION GENERAL I",
+    "level": 4,
+    "credits": 3.0,
+    "area": "DIRECCION CARRERA ING. CIVIL INFORMATICA",
+    "prerequisites": []
+  },
+  {
+    "id": "ING200",
+    "name": "TALLER DE DISEÑO DE INGENIERÍA",
+    "level": 4,
+    "credits": 6.0,
+    "area": "DECANATO FACULTAD DE INGENIERIA",
+    "prerequisites": ["ING101", "ING180"]
+  },
+  {
+    "id": "IME396",
+    "name": "PROBABILIDAD Y ESTADÍSTICA PARA INGENIERÍA",
+    "level": 4,
+    "credits": 5.0,
+    "area": "DEPTO. DE MATEMATICAS Y ESTADISTICA",
+    "prerequisites": ["IME298", "COD558"]
+  },
+  {
+    "id": "ICC505",
+    "name": "BASES DE DATOS",
+    "level": 4,
+    "credits": 4.0,
+    "area": "DEPTO. CS. COMPUTACION E INFORMATICA",
+    "prerequisites": ["ICC490", "COD558"]
+  },
+  {
+    "id": "ICC506",
+    "name": "INTERNET Y SISTEMAS OPERATIVOS",
+    "level": 4,
+    "credits": 6.0,
+    "area": "DEPTO. CS. COMPUTACION E INFORMATICA",
+    "prerequisites": ["ICC492", "COD558"]
+  },
+  {
+    "id": "ICC507",
+    "name": "ALGORITMOS Y PARADIGMAS",
+    "level": 4,
+    "credits": 6.0,
+    "area": "DEPTO. CS. COMPUTACION E INFORMATICA",
+    "prerequisites": ["ICC490", "COD558"]
+  }
 ];
+
