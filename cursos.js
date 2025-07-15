aba665a1f38a21cf6f2bbd7099634b33d741cb2c
Aantoooa <antonellabellof@gmail.com>
2025-07-14 22:04:51 -0400

    Create cursos.js

---
+++ cursos.js
@@ -0,0 +1,50 @@
+const COURSES = [
+  {
+    "id": "IME027",
+    "name": "PRECÁLCULO I",
+    "level": 1,
+    "credits": 3,
+    "area": "MATEMATICAS Y ESTADISTICA",
+    "prerequisites": []
+  },
+  {
+    "id": "IME028",
+    "name": "INTRODUCCIÓN AL ÁLGEBRA",
+    "level": 1,
+    "credits": 3,
+    "area": "MATEMATICAS Y ESTADISTICA",
+    "prerequisites": []
+  },
+  {
+    "id": "IME029",
+    "name": "RECÁLCULO II",
+    "level": 1,
+    "credits": 3,
+    "area": "MATEMATICAS Y ESTADISTICA",
+    "prerequisites": []
+  },
+  {
+    "id": "IME030",
+    "name": "ÁLGEBRA",
+    "level": 1,
+    "credits": 3,
+    "area": "MATEMATICAS Y ESTADISTICA",
+    "prerequisites": ["IME028"]
+  },
+  {
+    "id": "ICQ050",
+    "name": "INTRODUCCIÓN A LA QUÍMICA",
+    "level": 1,
+    "credits": 5,
+    "area": " CIENCIAS QUIMICAS Y REC. NAT.",
+    "prerequisites": [] 
+  },
+  {
+    "id": "DFI052",
+    "name": "HABILIDADES COMUNICATIVAS EN INGENIERÍA",
+    "level": 1,
+    "credits": 3,
+    "area": "COORDINACION FORMACIÓN GENERAL E IDIOMAS",
+    "prerequisites": [] 
+  }
+];
